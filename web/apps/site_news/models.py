24715d5d6334884cc77638cc1279e4ff1497b73e
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-25 17:41:15 +0300

    Create icon field in site_news django app

--- web/apps/site_news/models.py
+++ web/apps/site_news/models.py
@@ -5,3 +5,4 @@ class SiteNews(models.Model):
     title = models.CharField("Title of news", max_length=25)
     text = models.CharField("Not full text of news", max_length=120)
     date = models.DateField("Date of publish", default=timezone.now)
+    icon = models.CharField("Classes of icon", default="fa-solid fa-cubes", blank=True)
