40e9fddfa63985d2636061b7316638ad670764cb
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-25 08:26:05 +0300

    Fix: print() in settings.py, Makefile and etc.

--- web/apps/games_news/models.py
+++ web/apps/games_news/models.py
@@ -6,6 +6,5 @@ class GamesNews(models.Model):
     image = models.ImageField(
         "Upload an image of this artcile",
         upload_to="articles/")
-    mini_text = models.CharField("Not full text of article", max_length=120)
     full_text = models.TextField("Full text of article")
     date = models.DateField("Date of publish", default=timezone.now)
